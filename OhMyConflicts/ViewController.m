fa278e6e47b9b9c4673ee5ec936fa3b5c27f8a0f
Aaron Bradley <aaronb@Aarons-MacBook-Air.local>
2015-01-30 16:06:05 -0600

    initial commit

--- OhMyConflicts/ViewController.m
+++ OhMyConflicts/ViewController.m
@@ -16,7 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    ///chillin at the cut
+    NSString *string = @"yo yo yo";
+    NSString *string2 = @"awwww schnaaaaps!";
+    
 }
 
 - (void)didReceiveMemoryWarning {
